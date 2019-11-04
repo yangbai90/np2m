f0a6e4f2ef32f67649d3f54a7ec0e75d055c0608
walkandthinker <walkandthinker@gmail.com>
2019-11-04 15:18:18 +0100

    add cube and rectangle reader

--- src/GeoReader/PrintInfo.cpp
+++ src/GeoReader/PrintInfo.cpp
@@ -14,5 +14,18 @@ void GeoReader::PrintInfo()
     printf("***   tolerance=%14.6e, size=%14.6e            ***\n",_tolerance,_Size);
     printf("***   Input geo file= %-40s ***\n",_GeoFileName.c_str());
     printf("***   Output geo file= %-40s***\n",_NewGeoFileName.c_str());
+    switch(_JobType){
+    case JobType::RECTANGLE:
+        printf("***   LeftBC   lines=%8d, RightBC lines=%8d          ***\n",(int)_LeftLineIDSet.size(),(int)_RightLineIDSet.size());
+        printf("***   BottomBC lines=%8d, TopBC   lines=%8d          ***\n",(int)_BottomLineIDSet.size(),(int)_TopLineIDSet.size());
+        break;
+    case JobType::CUBIC:
+        printf("***   LeftBC   surfaces=%8d, RightBC surfaces=%8d    ***\n",(int)_LeftSurfaceIDSet.size(),(int)_RightSurfaceIDSet.size());
+        printf("***   BottomBC surfaces=%8d, TopBC   surfaces=%8d    ***\n",(int)_BottomSurfaceIDSet.size(),(int)_TopSurfaceIDSet.size());
+        printf("***   BackBC   surfaces=%8d, FrontBC surfaces=%8d    ***\n",(int)_BackSurfaceIDSet.size(),(int)_FrontSurfaceIDSet.size());
+        break;
+    default:
+        break;
+    }
     cout<<"******************************************************************"<<endl;
 }
